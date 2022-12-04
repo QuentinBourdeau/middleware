7683b4591fb7001a2288ec9e9d4dee3944acdfca
quentinBourdeau <quentin.bourdeau@etu.univ-cotedazur.fr>
2022-12-04 18:53:06 +0100

    main fixing

--- Java/LetsGoBiking/src/main/java/fr/unice/polytech/BikeProject/Main.java
+++ Java/LetsGoBiking/src/main/java/fr/unice/polytech/BikeProject/Main.java
@@ -7,31 +7,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("oui");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Rentrez l'adresse de votre point de départ : ");
-        String origin = sc.nextLine();
-        System.out.println("Rentrez l'adresse de votre point d'arrivée : ");
-        String destination = sc.nextLine();
-        while (origin.equals(destination)){
-            System.out.println("Vos adresse de départ et d'arrivée sont les mêmes!");
-            System.out.println("Rentrez l'adresse de votre point de départ : ");
-            origin = sc.nextLine();
-            System.out.println("Rentrez l'adresse de votre point d'arrivée : ");
-            destination = sc.nextLine();
+        System.out.println("What is your starting adress?");
+        String start = sc.nextLine();
+        System.out.println("What is your destination?");
+        String end = sc.nextLine();
+        while (start.equals(end)){
+            System.out.println("You're already here... both adresses are the same");
+            System.out.println("What is your starting adress?");
+            start = sc.nextLine();
+            System.out.println("What is your destination?");
+            end = sc.nextLine();
         }
 
         Biking applicationService = new Biking();
         IBiking service = applicationService.getBasicHttpBindingIBiking();
         try{
-            Itinerary itinerary = service.getItinerary(origin, destination);
+            Itinerary itinerary = service.getItinerary(start, end);
             for (Direction d : itinerary.getDirections().getValue().getDirection()) {
                 System.out.println(d.getProfile().getValue());
                 for (Step s : d.getSegment().getValue().getSteps().getValue().getStep()){
                     System.out.println("In " + s.getDistance().intValue() + " meters " + s.getInstruction().getValue());
                 }
             }
-            System.out.println("You have arrived!");}
+            System.out.println("You have arrived at your destination!");}
         catch (ServerSOAPFaultException exception) {System.out.println(exception.getMessage());}
     }
 }
