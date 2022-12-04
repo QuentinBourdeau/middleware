14aacc3ccbb603784d461658344b10900b9b3c0a
Vinh <narutovinh@hotmail.fr>
2022-12-04 16:58:06 +0100

    Test

--- SolutionsLab2/LetsGoBikingSelfHosted/Biking.cs
+++ SolutionsLab2/LetsGoBikingSelfHosted/Biking.cs
@@ -5,7 +5,7 @@ using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using LetsGoBikingSelfHosted.GenericProxyCache;
-
+//hahahah
 namespace LetsGoBikingSelfHosted
 {
     internal class Biking : IBiking
