a305d89635d53c84c372b2981a8015e5111d66a6
quentinBourdeau <quentin.bourdeau@etu.univ-cotedazur.fr>
2022-12-05 10:06:06 +0100

    added openroute call using location and no string

--- SolutionsLab2/LetsGoBikingSelfHosted/ApiOpenRoute.cs
+++ SolutionsLab2/LetsGoBikingSelfHosted/ApiOpenRoute.cs
@@ -71,5 +71,28 @@ namespace LetsGoBikingSelfHosted
                 }
             }
         }
+        public async Task<Rootobject> addressesToItinerary(Location startingPosition, Location endPosition, Boolean bicycle)
+        {
+            // exemple https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf6248b3387dbe4cbc4881a29750ec80c1b64a&start=8.681495,49.41461&end=8.687872,49.420318
+            string url = "https://api.openrouteservice.org/v2/directions/" + (bicycle ? "cycling-regular" : "foot-walking") + "?api_key=";
+            string text = "&start=" + startingPosition.lon + "," + startingPosition.lat + "&end=" +
+                endPosition.lon + "," + endPosition.lat + "&size=1";
+
+
+            var baseAddress = new Uri(url + API_KEY + text);
+
+            using (var httpClient = new HttpClient { BaseAddress = baseAddress })
+            {
+                httpClient.DefaultRequestHeaders.Clear();
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8");
+
+                using (var response = await httpClient.GetAsync(""))
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<Rootobject>(responseData);
+
+                }
+            }
+        }
     }
 }
